ad924559a8e3dd99bb16d386f3ec282c38d5db87
Sogol <oktavianramadhani25@gmail.com>
2024-05-26 15:18:37 +0700

    Ganti nama branch dan rapihin

--- coba.py
+++ coba.py
@@ -81,26 +81,26 @@ def knapsack_minuman():
     input(f"Barang diatas merupakan barang yang anda dapat dengan uang {uang_customer} Rupiah, Tekan enter untuk melanjutkan")
 
 def knapsack_01(items, capacity):
-        n = len(items)
-        dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
-        included_items = [[[] for _ in range(capacity + 1)] for _ in range(n + 1)]
+    n = len(items)
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    included_items = [[[] for _ in range(capacity + 1)] for _ in range(n + 1)]
 
-        for i in range(n + 1):
-            for w in range(capacity + 1):
-                if i == 0 or w == 0:
-                    dp[i][w] = 0
-                elif items[i - 1]["price"] <= w:
-                    if items[i - 1]["weight"] + dp[i - 1][w - items[i - 1]["price"]] > dp[i - 1][w]:
-                        dp[i][w] = items[i - 1]["weight"] + dp[i - 1][w - items[i - 1]["price"]]
-                        included_items[i][w] = included_items[i - 1][w - items[i - 1]["price"]] + [items[i - 1]]
-                    else:
-                        dp[i][w] = dp[i - 1][w]
-                        included_items[i][w] = included_items[i - 1][w]
+    for i in range(n + 1):
+        for w in range(capacity + 1):
+            if i == 0 or w == 0:
+                dp[i][w] = 0
+            elif items[i - 1]["price"] <= w:
+                if items[i - 1]["weight"] + dp[i - 1][w - items[i - 1]["price"]] > dp[i - 1][w]:
+                    dp[i][w] = items[i - 1]["weight"] + dp[i - 1][w - items[i - 1]["price"]]
+                    included_items[i][w] = included_items[i - 1][w - items[i - 1]["price"]] + [items[i - 1]]
                 else:
                     dp[i][w] = dp[i - 1][w]
                     included_items[i][w] = included_items[i - 1][w]
+            else:
+                dp[i][w] = dp[i - 1][w]
+                included_items[i][w] = included_items[i - 1][w]
 
-        return dp[n][capacity], included_items[n][capacity]
+    return dp[n][capacity], included_items[n][capacity]
 
 
 
